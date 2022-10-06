765286ff31361bc23fd3a8909195b7c8ecf7c79e
Velzeb <113321553+Velzeb@users.noreply.github.com>
2022-10-06 12:02:33 -0400

    C3Carlos

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@
     <img
       alt="Vue logo"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Adobe_XD_CC_icon.svg/250px-Adobe_XD_CC_icon.svg.png"
+      
     />
     <HelloWorld msg=" Welcome to Your Vue.js App" />
-
+    <img
+          alt="Vue"
+      src="https://image.shutterstock.com/image-vector/vector-creative-illustration-cosmonaut-spacesuit-600w-1680589189.jpg"
+    />
     <HelloWorld msg=" Las tecnologías Web son lo Máximo" />
 
   </div>
