7968790cd156ba8009ed634a3115f74a7c925266
MRK2705 <86754297+MRK2705@users.noreply.github.com>
2022-10-06 12:14:18 -0400

    C4Mirko

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,9 @@
     <img
       alt="Vue logo"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Adobe_XD_CC_icon.svg/250px-Adobe_XD_CC_icon.svg.png"
-      
     />
     <HelloWorld msg=" Welcome to Your Vue.js App" />
-    <img
-          alt="Vue"
-      src="https://image.shutterstock.com/image-vector/vector-creative-illustration-cosmonaut-spacesuit-600w-1680589189.jpg"
-    />
-    <HelloWorld msg=" Las tecnologías Web son lo Máximo" />
-
+    <HelloWorld msg="Pruebas De Vue con GitHub" />
   </div>
 </template>
 
